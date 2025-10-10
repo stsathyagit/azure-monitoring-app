7f0a13e947e63e26c0e3b4835660c87253279520
stsathya <stsathya_btech@yahoo.co.in>
2025-10-11 00:46:28 +0100

    given id directly

--- frontend/src/authConfig.js
+++ frontend/src/authConfig.js
@@ -1,7 +1,7 @@
 export const msalConfig = {
   auth: {
-    clientId: process.env.REACT_APP_CLIENT_ID || "<REPLACE_WITH_CLIENT_ID>",
-    authority: `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID || "<REPLACE_WITH_TENANT_ID>"}`,
+    clientId: process.env.REACT_APP_CLIENT_ID || "aad35dbf-35f4-4447-b615-4d1a99798955",
+    authority: `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID || "8d542808-5b3a-4b7c-ba4b-7e285151a589"}`,
     redirectUri: window.location.origin
   },
   cache: {
